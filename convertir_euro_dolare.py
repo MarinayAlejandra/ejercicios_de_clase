fccc1177112075cb151fb2dcf258688d8bd97492
MarinayAlejandra <martin-loeches.pineda.marina@iescamas.es>
2024-09-30 10:10:20 +0200

    Add files via upload

---
+++ convertir_euro_dolare.py
@@ -0,0 +1,10 @@
+# Esto es un programa que convierte euros en dólares
+
+print ("dime una cantidad: ")
+num1= int(input())
+num2=float(1.10)
+
+# Realizamos la operación 
+total= num1*num2
+
+print ("El resultado de sumar", num1 , "*", num2 , "=", total)
---
+++ entrada-salida.py
@@ -0,0 +1,12 @@
+# Esto es un programa que suma dos números
+print ("dime un número: ")
+num1 = int(input())
+
+print ("dime otro número: ")
+num2 =int( input())
+
+# Realizamos la operación de suma
+
+total = num1 + num2
+
+print ("El resultado de sumar", num1 , "+", num2 , "=", total)
