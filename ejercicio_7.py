9bebc85e1b05c7bd7e876478275069278f07eb07
MarinayAlejandra <martin-loeches.pineda.marina@iescamas.es>
2024-10-07 10:04:55 +0200

    Add files via upload

---
+++ ejercicio_7.py
@@ -0,0 +1,17 @@
+# este programa convierte una longitud a partir de metros
+
+'''
+Escribe un programa que calcule 
+el una unidad de una persona 
+pidiéndole su peso en kilogramos y su altura en metros.
+'''
+
+print ("Dime la nota")  
+nota1 = float(input())
+print("dime la nota2")
+nota2 = float(input())
+print("dime la nota3")
+nota3 = float(input())
+print(nota3)
+media = (nota1+nota2+nota3)/3
+print("el resultado de la nota es", media)
